94fd6eb12d34da6e451163a4c795ccde75d2b3d4
Subhankar Das <94730919+subhankardas2000@users.noreply.github.com>
2024-03-26 23:40:18 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "web designing", "android development", "web development"],
+    strings: ["web development", "computer Network ", "Operating systems", "python","java","Database management"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
